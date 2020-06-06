48a3a86de8ff752ce8f3e599e4e921e47f97b1f3
Ailton Lima de Andrade <ailtonlm.andrade@outlook.com>
2020-06-06 01:16:13 -0300

    Seção 04 - Correcao-Exercicios-Fixacao

---
+++ Correcao-Exercicios-Fixacao/Correcao-Exercicios-Fixacao/ConversorDeMoeda.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Correcao_Exercicios_Fixacao
+{
+    class ConversorDeMoeda
+    {
+        public static double Iof = 6.0;
+
+        public static double DolarParaReal(double quantiaDolar, double cotacaoDolar) 
+        {
+            double totalReal = quantiaDolar * cotacaoDolar;
+            return totalReal + totalReal *  Iof / 100.0;
+        }
+    }
+}
