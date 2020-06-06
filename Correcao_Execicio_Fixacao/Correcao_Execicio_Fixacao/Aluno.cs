21c3155c83774afa71467af3ce2954b509b020e4
Ailton Lima de Andrade <ailtonlm.andrade@outlook.com>
2020-06-05 23:54:46 -0300

    Sessão 04

---
+++ Correcao_Execicio_Fixacao/Correcao_Execicio_Fixacao/Aluno.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace Correcao_Execicio_Fixacao
+{
+    class Aluno
+    {
+        public string Nome;
+        public double[] Notas = new double[3];
+        double totalNotas;
+
+        public double SomaNotas()
+        {
+            double soma = 0;
+
+            Console.WriteLine("Digite as três notas dos alunos:");
+
+            for (int iIndiceNotas = 0; iIndiceNotas < 3; iIndiceNotas++)
+            {
+                Notas[iIndiceNotas] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                soma += Notas[iIndiceNotas];
+            }
+
+            return soma;
+        }
+
+        public bool VerificaStatus()
+        {
+            totalNotas = SomaNotas();
+
+            if (totalNotas >= 60)
+                return true;
+            else
+                return false;
+        }
+
+        public double CalculaRestante()
+        {
+            return 60 - totalNotas; ;
+        }
+
+        public override string ToString()
+        {
+
+            if (VerificaStatus() == true)
+            {
+                return "NOTA FINAL = " + totalNotas.ToString("F2", CultureInfo.InvariantCulture) + "\n"
+                        + "APROVADO" ;
+            }
+            else 
+            {
+                return "NOTA FINAL = " + totalNotas.ToString("F2", CultureInfo.InvariantCulture) + "\n"
+                        + "REPROVADO" + "\n"
+                        + "FALTARAM " + CalculaRestante().ToString("F2", CultureInfo.InvariantCulture) + " PONTOS";
+            }
+
+            
+        }
+
+    }
+}
