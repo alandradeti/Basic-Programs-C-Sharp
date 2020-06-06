becb7aa2231c111600fba17f3340139204cbce29
Ailton Lima de Andrade <ailtonlm.andrade@outlook.com>
2020-06-06 00:04:18 -0300

    Alteração do nome da pasta

--- Correcao-exercicios-fixacao/Correcao-exercicios-fixacao/obj/Debug/netcoreapp3.1/Correcao-Exercicios-Fixacao1.AssemblyInfo.cs
+++ Correcao-exercicios-fixacao/Correcao-exercicios-fixacao/obj/Debug/netcoreapp3.1/Correcao-Exercicios-Fixacao1.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Correcao-Exercicios-Fixacao")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Correcao-Exercicios-Fixacao1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Correcao-Exercicios-Fixacao")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Correcao-Exercicios-Fixacao")]
+[assembly: System.Reflection.AssemblyProductAttribute("Correcao-Exercicios-Fixacao1")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Correcao-Exercicios-Fixacao1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
