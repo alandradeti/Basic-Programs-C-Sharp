26f954ab6af617982618b9400272e3dba293366e
Ailton Lima de Andrade <ailtonlm.andrade@outlook.com>
2020-06-12 18:34:05 -0300

    Exercicio-Fixacao-Secao-06

---
+++ Exercicio-Fixacao-Secao-06/Exercicio-Fixacao-Secao-06/Estudante.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercicio_Fixacao_Secao_06
+{
+    class Estudante
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+        public Estudante(string nome, string email)
+        {
+            Nome = nome;
+            Email = email;
+        }
+
+        public override string ToString()
+        {
+            return Nome + ", " + Email;
+        }
+    }
+}
