2fb34bfd3606491a7d02918ff5880604e392d676
Ailton Lima de Andrade <ailtonlm.andrade@outlook.com>
2020-06-20 16:07:22 -0300

    Exercicio-Fixacao-Lista-Secao-06

---
+++ Exercicio-Fixacao-Listas-Secao-06/Exercicio-Fixacao-Listas-Secao-06/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio_Fixacao_Listas_Secao_06
+{
+    class Employee
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; private set; }
+
+        public Employee(string id, string name, double salary)
+        {
+            Id = id;
+            Name = name;
+            Salary = salary;
+        }
+
+        //Method that increases the salary
+        public void IncreaseSalary(double percentage)
+        {
+            Salary += Salary * percentage / 100.0;
+        }
+
+        public override string ToString()
+        {
+            return Id + ", " + Name + ", " + Salary.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Exercicio-Fixacao-Listas-Secao-06/Exercicio-Fixacao-Listas-Secao-06/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace Exercicio_Fixacao_Listas_Secao_06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int quantityEmployee;
+            string searchId_employee;
+
+            //Defining Employee type list
+            List<Employee> listEmployee = new List<Employee>();
+             
+            Console.Write("How many employees will be registered? ");
+            quantityEmployee = int.Parse(Console.ReadLine());
+
+            for (int counter = 1; counter <= quantityEmployee; counter++) 
+            {
+                Console.WriteLine("Employee #{0}:", counter);
+
+                Console.Write("Id: ");
+                string id_employee = Console.ReadLine();
+
+                Console.Write("Name: ");
+                string name_employee = Console.ReadLine();
+
+                Console.Write("Salary: ");
+                double salary_employee = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                //Adding Employee object data to the Employee type list
+                listEmployee.Add(new Employee(id_employee, name_employee, salary_employee));
+
+                Console.WriteLine("");
+            }
+            
+            Console.Write("\nEnter the employee id that will have salary increase: ");
+            searchId_employee = Console.ReadLine();
+
+            //Employee such that Id_employee is equal to searchId
+            Employee searchEmployee = listEmployee.Find(employee => employee.Id == searchId_employee);
+            if (searchEmployee != null)
+            {
+                Console.Write("Enter the percentage: " );
+                double percentageIncrease = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                searchEmployee.IncreaseSalary(percentageIncrease);
+
+            }
+            else 
+            {
+                Console.WriteLine("This employee does not exist!");
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("Update list employees:");
+
+            //Loop to scroll through the list of employees
+            foreach (Employee employee in listEmployee)
+            {
+                Console.WriteLine(employee);
+            }
+
+
+        }
+    }
+}
