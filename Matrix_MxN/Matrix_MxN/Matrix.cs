7f986f003f5154418959d0cb273e1ce646bfa3da
Ailton Lima de Andrade <ailtonlm.andrade@outlook.com>
2020-07-04 12:12:45 -0300

    PRG - (Matrix_MxN)

---
+++ Matrix_MxN/Matrix_MxN/Matrix.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Matrix_MxN
+{
+    class Matrix
+    { 
+
+        public int Line { get; private set; }
+
+        public int Column { get; private set; }
+
+        public Matrix(int line, int column)
+        {
+            Line = line;
+            Column = column;
+        }
+
+        //Performs the reading of the matrix and returns the resulting matrix
+        public int[,] ReadMatrix()
+        {
+            int[,] matrix = new int[Line, Column];
+
+            for (int l = 0; l < Line; l++)
+            {
+                string[] values = Console.ReadLine().Split(' ');
+
+                for (int c = 0; c < Column; c++)
+                {
+                    matrix[l, c] = int.Parse(values[c]);
+                }
+            }
+
+            return matrix;
+        }
+
+        //Returns the position of the number entered in the matrix and the numbers around it
+        public void PositionMatrix(int[,] matrix, int number)
+        {
+            for (int l = 0; l < Line; l++)
+            {
+                for (int c = 0; c < Column; c++)
+                {
+                    if (matrix[l, c] == number)
+                    {
+                        Console.WriteLine("Position: " + l + "," + c);
+
+                        if (l > 0) Console.WriteLine("Up: " + matrix[l - 1, c]); //Print the number above the "number"
+                        if (c > 0) Console.WriteLine("Left: " + matrix[l, c - 1]); //Print the number to the left of the "number"
+                        if (c < Column - 1) Console.WriteLine("Right: " + matrix[l, c + 1]); //Print the number to the right of the "number"
+                        if (l < Line - 1) Console.WriteLine("Down: " + matrix[l + 1, c]); //Print the number below the "number"
+                    }
+                }
+            }
+        }
+
+    }
+}
---
+++ Matrix_MxN/Matrix_MxN/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Matrix_MxN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int m, n, number;
+            int[,] resultMatrix;
+            Matrix matrix;
+
+            Console.Write("Enter the number of lines: ");
+            m = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the number of columns: ");
+            n = int.Parse(Console.ReadLine());
+
+            matrix = new Matrix(m, n);
+
+            resultMatrix = matrix.ReadMatrix();
+
+            Console.Write("Enter an integer: ");
+            number = int.Parse(Console.ReadLine());
+
+            matrix.PositionMatrix(resultMatrix, number);
+
+
+        }
+    }
+}
